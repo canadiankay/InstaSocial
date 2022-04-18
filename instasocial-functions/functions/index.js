ca58391b9d0a5cf3c51289932d5721f74340de85
canadiankay <kasimnadia@gmail.com>
2022-04-18 23:54:25 +0000

    first commit- add app and db folder

---
+++ instasocial-functions/functions/index.js
@@ -0,0 +1,19 @@
+const functions = require("firebase-functions");
+const admin = require('firebase-admin');
+
+admin.initializeApp();
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  functions.logger.info("Hello logs!", {structuredData: true});
+  response.send("Hello World!");
+});
+
+// fetch data from database and
+exports.getScreams = functions.https.onRequest((request, response) => {
+
+
+
+})
